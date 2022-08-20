823a838eb660ab8e5f640b5b91783716788f3cbc
/azman <am911444@gmail.com>
2022-08-20 13:09:54 +0700

    commit pertama

---
+++ database/migrations/2022_08_17_031050_create_kelas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKelasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_kelas')->nullable();
+            $table->string('image')->nullable();
+            $table->string('content')->nullable();
+            $table->string('urutan')->nullable();
+            $table->string('kategoris_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+}
